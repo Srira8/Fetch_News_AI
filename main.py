e64eb1062bae83b466b14a0d0c9d4839a06ad8b5
Srira8 <129417936+Srira8@users.noreply.github.com>
2026-05-27 16:01:51 -0700

    Update main.py
    Required comments

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-# test
+# Entry point for the news aggregation pipeline
+# Fetches headlines and persists them to disk
+
 from gatherers.news_scraper import fetch_news
 from utils.file_manager import save_data
 
